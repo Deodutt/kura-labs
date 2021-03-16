8e94275b7f7db2d3abcde975f99013c96ae98bf9
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-15 21:12:53 -0400

    Typeof and type conversion functions

---
+++ Dev Learning Assignment/python-files/03-typeconversion.py
@@ -0,0 +1,14 @@
+# birth_year = input("Birth year: ")
+# print(type(birth_year))
+
+# age = 2021 - int(birth_year)
+# print(type(age))
+# print(age)
+
+
+# exercise: ask a user their weight (in pounds), convert it to kilograms and print on the terminal
+
+weight = input("How much do you weigh(lbs)? ")
+converted = float(weight) * 0.45359237
+
+print(f"You weight {converted} kilograms.")

e3a2e89bdf20f9afc7a8e8421b2ff231804caf37
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-15 21:12:15 -0400

    Input function

---
+++ Dev Learning Assignment/python-files/02-input.py
@@ -0,0 +1,13 @@
+# name = input("What is your name? ")
+# # input function will print the prompt onto the terminal and then wait for user to enter a value. whatever is entereted, the input function is returned. you can store it by making it equals a varaible
+
+# print("Hi " + name)  # another example of an expression
+
+
+# exercise
+
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+
+print(name + " likes " + color)
+# print(f"{name} likes {color}")

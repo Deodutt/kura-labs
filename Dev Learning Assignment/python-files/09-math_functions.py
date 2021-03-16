53f2a5e618a42540480b63a2629eb488f35e9d22
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-16 01:24:01 -0400

    Math Functions

---
+++ Dev Learning Assignment/python-files/09-math_functions.py
@@ -0,0 +1,15 @@
+import math
+
+# now we can access the math functions using the . method
+
+x = 2.9
+
+# to round number, use built in function
+print(round(x))
+
+
+# built in fucntion called ABS. absoulte function. value given and always return positive #.
+print(round(-2.8))
+
+print(math.ceil(2.9))  # this gets the ceiling of a number basically rounds up
+print(math.floor(2.9))  # this gets the floor of a number basically rounds down

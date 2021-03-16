fc9b48a52f5c87ae9561988fb3f67b95a2ca2920
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-15 21:11:42 -0400

    First python file that focused on variables

---
+++ Dev Learning Assignment/python-files/01-variables.py
@@ -0,0 +1,22 @@
+# print("Ricardo Deodutt")
+# print("o----")
+# print(" ||||")
+# print("*" * 10)
+
+
+price = 10
+# 10 will be changed into binary
+
+price = 20  # integer
+# price will be 20 because python interpreter reads line by line
+print(price)
+rating = 4.9  # float
+name = "Mosh"  # string
+
+is_published = False  # boolean
+is_published = True  # boolean
+
+# EXERCISE
+name = "John Smith"
+age = 20
+new_patient = True

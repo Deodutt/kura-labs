68f70f104184e970595bd2cae7225a8fec3ba973
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-16 00:04:58 -0400

    Arithmetic Operators

---
+++ Dev Learning Assignment/python-files/07-arithmetic_operations.py
@@ -0,0 +1,19 @@
+10  # integers
+10.123  # flaot
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)  # floating point number
+print(10 // 3)  # returns an integer 3
+print(10 % 3)  # modulus returns remainder of the division. returns 1
+print(10 ** 3)  # modulus returns remainder of the division. returns 1
+
+x = 10
+# increment by 3
+x = x + 3
+print(x)
+
+# augmented assignment operator lets us write the same code in a shorter form
+x = +3
+print(x)

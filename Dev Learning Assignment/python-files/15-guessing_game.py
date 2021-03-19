97c130c63d1f783be3fde275d79aacfa99ef41d0
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-19 02:05:34 -0400

    Guessing Game Exercise

---
+++ Dev Learning Assignment/python-files/15-guessing_game.py
@@ -0,0 +1,14 @@
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+
+while guess_count < guess_limit:
+    user_guess = int(input("Guess: "))
+    guess_count += 1
+
+    if user_guess == secret_number:
+        print("You Won!")
+        break
+
+else:
+    print("You lost")

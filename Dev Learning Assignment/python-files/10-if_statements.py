ad4a428ad3527cb718ec9b6ec5980e64b5155da1
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-19 00:41:53 -0400

    If Statements

---
+++ Dev Learning Assignment/python-files/10-if_statements.py
@@ -0,0 +1,33 @@
+# is_hot = False
+# is_cold = False
+
+
+# if is_hot == True:
+#     print("It's a hot day!")
+#     print("Drink plenty of water")
+
+# elif is_cold:
+#     print("It's a cold day!")
+#     print("Wear some warm clothing!")
+
+# else:
+#     print("It is a lovely day!")
+
+# print("Enjoy your day!")
+
+
+# -----
+
+# exercise
+
+price = 1000000
+
+is_good_credit = True
+
+if is_good_credit == True:
+    down_payment = 0.1 * price
+
+else:
+    down_payment = 0.2 * price
+
+print(f"Down payment: ${down_payment}")

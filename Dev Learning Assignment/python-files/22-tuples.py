2e81c1e49fdee8f7325215e648bb4c8b7db5c3a8
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-22 01:56:17 -0400

    Tuples

---
+++ Dev Learning Assignment/python-files/22-tuples.py
@@ -0,0 +1,13 @@
+# numbers = [1, 2, 3] #list
+
+numbers = (1, 2, 3)  # tuples
+numbers.count  # used to count the number of occurances of an item
+numbers.index  # to find index of the first occurance of that item.
+
+
+# We can only get information from a tuple. You cannot change it.
+
+# Just like list you can access each item in the list
+numbers(1, 2, 3)
+numbers[0] = 10
+print(numbers[0])

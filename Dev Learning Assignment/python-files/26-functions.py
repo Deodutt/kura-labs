610c3597c7604f0d54b76ba7eb362cd13eb08ee9
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-22 21:06:19 -0400

    Functions

---
+++ Dev Learning Assignment/python-files/26-functions.py
@@ -0,0 +1,9 @@
+# def is a resevered key word in python. when intrepeter sees this they know we are defining a function.
+def greet_user():
+    print("Hi there!")
+    print("Welcome aboard!")
+
+
+print("Start")
+greet_user()
+print("Finish")

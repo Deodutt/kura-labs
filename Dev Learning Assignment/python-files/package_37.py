f9fd8d27c61fbdb8ed81a5d72b05eb38b7f87535
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-03-23 21:28:06 -0400

    Packages

---
+++ Dev Learning Assignment/python-files/package_37.py
@@ -0,0 +1,9 @@
+# import ecommerce_package.shipping
+
+# ecommerce_package.shipping.calc_shipping()
+
+# from ecommerce_package.shipping import calc_shipping
+# calc_shipping()
+
+
+from ecommerce_package import shipping
